15f6e4200f342a4e098b541f3c7aae1f4c9a5174
Michael Smethurst <michael@smethur.st>
2020-07-02 19:11:19 +0100

    removed end date, made procedure a name not id

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -3,13 +3,13 @@ drop table if exists instruments;
 create table instruments (
 	id serial,
 	title varchar(10000) not null,
-	procedure_id varchar(30) not null,
+	procedure varchar(100) not null,
 	laying_body varchar(10000) not null,
 	date_made date not null,
 	date_laid date not null,
-	end_date date not null,
 	instrument_uri varchar(100) not null,
 	work_package_uri varchar(100) not null,
+	tna_uri varchar(200) not null,
 	is_tweeted boolean default false,
 	primary key (id)
 );
