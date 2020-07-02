d5994eeadca8413c4de2ca107ca89d64d4b9a1e2
Michael Smethurst <michael@smethur.st>
2020-07-02 19:10:56 +0100

    composed tweet text

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,2 +1,33 @@
 class Instrument < ActiveRecord::Base
+  
+  def tweet_text
+    tweet_text = ''
+    tweet_text = tweet_text + self.short_title
+    tweet_text = tweet_text + '. Made on '
+    tweet_text = tweet_text + self.date_made.strftime( '%d-%m-%Y')
+    tweet_text = tweet_text + ', laid on '
+    tweet_text = tweet_text + self.date_laid.strftime( '%d-%m-%Y')
+    tweet_text = tweet_text + '. Subject to the '
+    tweet_text = tweet_text + self.procedure
+    tweet_text = tweet_text + ' procedure. '
+    tweet_text = tweet_text + self.url
+    tweet_text
+  end
+  
+  def short_title
+    if self.title.length > 200
+      title = self.title[0, 198] + '..'
+    else
+      title = self.title
+    end
+    title
+  end
+  
+  def url
+    'https://statutoryinstruments.parliament.uk/timeline/' + self.instrument_id + '/madenlaid/'
+  end
+  
+  def instrument_id
+    self.instrument_uri.split( '/' ).last
+  end
 end
