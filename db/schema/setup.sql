cd5a31edc7364458dce3fc93279a48f2cc4f4ddb
Michael Smethurst <michael@smethur.st>
2020-07-02 14:39:12 +0100

    added made date

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -5,6 +5,7 @@ create table instruments (
 	title varchar(10000) not null,
 	procedure_id varchar(30) not null,
 	laying_body varchar(10000) not null,
+	date_made date not null,
 	date_laid date not null,
 	end_date date not null,
 	instrument_uri varchar(100) not null,
