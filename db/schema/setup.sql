3f0ef37ba037d5038fd1d091b0c51f6bcc08f1b2
Michael Smethurst <michael@smethur.st>
2020-07-02 11:19:30 +0100

    schema setup

---
+++ db/schema/setup.sql
@@ -0,0 +1,14 @@
+drop table if exists instruments;
+
+create table instruments (
+	id serial,
+	title varchar(10000) not null,
+	procedure_id varchar(30) not null,
+	laying_body varchar(10000) not null,
+	date_laid date not null,
+	end_date date not null,
+	instrument_uri varchar(100) not null,
+	work_package_uri varchar(100) not null,
+	is_tweeted boolean default false,
+	primary key (id)
+);
